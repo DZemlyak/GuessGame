5113019546f66800788e25ca99891403ff8393be
Dmitriy Zemlyak <dmitriy.zemlyak@hotmail.com>
2015-01-30 15:29:54 +0200

    GuessGame

---
+++ GuessGame/Controllers/HomeController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuessGame.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string Host = "Host";
+        private const string Winner = "Winner";
+        private const string SecretNumber = "SecretNumber";
+        private const string History = "History";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult GetStatus() {
+            var host = System.Web.HttpContext.Current.Application.Get(Host);
+            if (host != null)
+                return Json(new {
+                    Host = host
+                }, JsonRequestBehavior.AllowGet);
+
+            var winner = System.Web.HttpContext.Current.Application.Get(Winner);
+            if (winner == null)
+                return Json(new {
+                    Status = "The Game hasn't started yet. Start new Game!"
+                }, JsonRequestBehavior.AllowGet);
+
+            var secretNumber = System.Web.HttpContext.Current.Application.Get(SecretNumber);
+            return Json(new {
+                Winner = winner,
+                SecretNumber = secretNumber
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GuessNumber(int number, string userName) {
+            var host = System.Web.HttpContext.Current.Application.Get(Host);
+            string result;
+
+            if (host == null)
+                return Json(new {
+                    Result = "The Game hasn't started yet!"
+                }, JsonRequestBehavior.AllowGet);
+
+            if ((string) host == userName)
+                return Json(new {
+                    Result = "You can't play!"
+                }, JsonRequestBehavior.AllowGet);
+            
+            var secretNumber = (int) System.Web.HttpContext.Current.Application.Get(SecretNumber);
+            if (number > secretNumber)
+                result = "Your number is bigger!";
+            else if (number < secretNumber)
+                result = "Your number is smaller!";
+            else {
+                result = "Right! You win!";
+                System.Web.HttpContext.Current.Application.Set(Host, null);
+                System.Web.HttpContext.Current.Application.Set(Winner, userName);
+            }
+            
+            var history = ((Dictionary<string, List<int>>) System.Web.HttpContext.Current.Application.Get(History));
+            history[userName].Add(number);
+
+            return Json(new {
+                Result = result
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult StartGame(int number, string userName) {
+            System.Web.HttpContext.Current.Application.Set(Host, userName);
+            System.Web.HttpContext.Current.Application.Set(SecretNumber, number);
+            System.Web.HttpContext.Current.Application.Set(History, new Dictionary<string, List<int>>());
+            return Json(new {
+                Result = ""
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public void SetHistory(string userName)
+        {
+            ((Dictionary<string, List<int>>)System.Web.HttpContext.Current.Application.Get(History))
+                .Add(userName, new List<int>());
+        }
+
+        public ActionResult ShowHistory(string userName)
+        {
+            var history = String.Empty;
+            var historyValues = ((Dictionary<string, List<int>>)System.Web.HttpContext.Current.Application.Get(History));
+            history = historyValues[userName].Aggregate(history, (current, number) => current + number + ", ");
+            history = history.Substring(0, history.Length - 2);
+            return Json(new {
+                History = "Your guesses: " + history
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public void SetAllHistory()
+        {
+            if (System.Web.HttpContext.Current.Application.Get(History) == null)
+                System.Web.HttpContext.Current.Application.Set(History, new Dictionary<string, List<int>>());
+        }
+    }
+}
---
+++ GuessGame/Scripts/game.js
@@ -0,0 +1,141 @@
+﻿$(document).ready(function() {
+ 
+    var $logInButton = $("#logInButton");
+    var $logOutButton = $("#logOutButton");
+    var $guessButton = $("#guessButton");
+    var $newGameButton = $("#newGameButton");
+    var $showHistory = $("#showHistory");
+
+    enableHistory();
+    checkCookie();
+    getStatus();
+
+    $logInButton.click(function () {
+        setCookie();
+        checkCookie();
+    });
+
+    $logOutButton.click(function () {
+        $.removeCookie("UserName");
+        checkCookie();
+    });
+
+    $guessButton.click(function() {
+        guessNumber();
+    });
+
+    $newGameButton.click(function () {
+        startGame();
+    });
+
+    $showHistory.click(function () {
+        showHistory();
+    });
+
+});
+
+function enableHistory() {
+    $.ajax({
+        url: "/Home/SetAllHistory",
+        method: "POST"
+    });
+}
+
+function checkCookie() {
+    var cookieValue = $.cookie("UserName");
+    if (typeof cookieValue === "undefined") {
+        showElements(false);
+    } else {
+        var $greetings = $("#userGreeting");
+        showElements(true);
+        var text = "Hello, " + cookieValue + "!";
+        $greetings[0].innerHTML = text;
+
+        $.ajax({
+            url: "/Home/SetHistory?userName=" + cookieValue,
+            method: "POST"
+        });
+    }
+}
+
+function setCookie() {
+    var $name = $("#userName").val().trim();
+    $.cookie("UserName", $name,{
+        expires: 1,
+        path: '/'
+    });
+}
+
+function showElements(logged) {
+    var $logOut = $("#logOut");
+    var $logIn = $("#logIn");
+    var $field = $("#gameField");
+    if (logged) {
+        $logOut.show();
+        $logIn.hide();
+        $field.show();
+    } else {
+        $logOut.hide();
+        $logIn.show();
+        $field.hide();
+    }
+}
+
+function getStatus() {
+    var $gameStatus = $("#gameStatus");
+    $.ajax({
+        url: "/Home/GetStatus",
+        method: "GET",
+        success: function(data) {
+            if (data.Host != null) {
+                $gameStatus.text("User \"" + data.Host + "\" has started the Game. Guess the Number!");
+            } else if(data.Winner != null) {
+                $gameStatus.text("User \"" + data.Winner + "\" is a winner! The Number was " + data.SecretNumber + ".");
+            } else {
+                $gameStatus.text(data.Status);
+            }
+            setTimeout(getStatus, 1000);
+        },
+        error: function() {
+            $gameStatus.text("Error occurred!");
+        }
+    });
+}
+
+function guessNumber() {
+    var $number = parseInt($("#guessNumberInput").val().trim());
+    var $result = $("#userResult");
+    var $userName = $.cookie("UserName");
+    $.ajax({
+        url: "/Home/GuessNumber?number=" + $number + "&userName=" + $userName,
+        method: "GET",
+        success: function (data) {
+            $result.text(data.Result);
+        }
+    });
+}
+
+function startGame() {
+    var $number = parseInt($("#startGameInput").val().trim());
+    var $userName = $.cookie("UserName");
+    $.ajax({
+        url: "/Home/StartGame?number=" + $number + "&userName=" + $userName,
+        method: "GET",
+        success: function () {
+            $("#startGameInput").text("");
+        }
+    });
+}
+
+function showHistory() {
+    var $history = $("#history");
+    var $userName = $.cookie("UserName");
+    $.ajax({
+        url: "/Home/ShowHistory?userName=" + $userName,
+        method: "GET",
+        success: function (data) {
+            $history.text(data.History);
+        }
+    });
+
+}
